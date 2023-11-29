319a1a87fe048d9c116d122553d95760c79dbeb7
Tom5521 <mismarttv321@gmail.com>
2023-11-28 23:43:51 -0300

    Add builder with shared libraries to improve the file size!

--- build.sh
+++ build.sh
@@ -6,6 +6,10 @@ go build -o builds/arm64/ArchInstaller-arm64 .
 echo building for amd64
 export GOARCH=amd64
 go build -o builds/amd64/ArchInstaller-amd64 .
+echo Building with dynamic libraries
+sudo go build -o builds/amd64/ArchInstaller-amd64-dl -linkshared .
 echo building for 386
 export GOARCH=386
 go build -o builds/i386/ArchInstaller-i386 .
+echo Building with dynamic libraries
+go build -o builds/i386/ArchInstaller-i386-dl -linkshared .
